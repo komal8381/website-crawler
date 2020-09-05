a07568277a9790a9398dc39d9af9f1fb1ec51f81
kgarg <komal.garg@here.com>
2020-09-05 23:41:26 +0530

    Added gitignore

--- Crawler_project/src/main/scala/ASN.scala
+++
@@ -1,8 +0,0 @@
-/**
-  * ASN class
-  */
-case class ASN(country:String,name:String,registry:String,num_ip_addresses:Int){
-  override def toString(): String ={
-    country + "," + name + "," + registry + "," + num_ip_addresses
-  }
-}
